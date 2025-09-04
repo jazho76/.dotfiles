8da3a61c790b99388cbcde4a6aba7437e576b334
joaquin <jpinilloslr@gmail.com>
2025-09-04 13:14:22 +0200

    add pyenv config

---
+++ bash/.bashrc.d/pyenv.sh
@@ -0,0 +1,25 @@
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+
+PATH="$(bash --norc -ec 'IFS=:; paths=($PATH);
+for i in ${!paths[@]}; do
+if [[ ${paths[i]} == "''/home/joaquin/.pyenv/shims''" ]]; then unset '\''paths[i]'\'';
+fi; done;
+echo "${paths[*]}"')"
+export PATH="${HOME}/.pyenv/shims:${PATH}"
+export PYENV_SHELL=bash
+source "${HOME}/.pyenv/completions/pyenv.bash"
+command pyenv rehash 2>/dev/null
+
+pyenv() {
+  local command=${1:-}
+  [ "$#" -gt 0 ] && shift
+  case "$command" in
+  activate|deactivate|rehash|shell)
+    eval "$(pyenv "sh-$command" "$@")"
+    ;;
+  *)
+    command pyenv "$command" "$@"
+    ;;
+  esac
+}
